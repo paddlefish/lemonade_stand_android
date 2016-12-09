0fccd551b34b57d20bb114f208ca7570f64ab92a
Andrew Rahn <andy@iconfactory.com>
2016-12-09 09:40:04 -0600

    Stub in a place for exercise to finish ListView via SimpleAdapter

--- app/src/main/java/net/paddlefish/lemonadestand/RulesActivity.java
+++ app/src/main/java/net/paddlefish/lemonadestand/RulesActivity.java
@@ -6,8 +6,43 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RulesActivity extends AppCompatActivity {
+
+	/**
+	 * Provides a list of rules, ready to be fed into SimpleAdapter
+	 * Each entry in the list has these "from" keys
+	 * "rule" -- the rule written out.
+	 * "index" -- a string containing the rule number (starting with 1)
+	 * "icon" -- a java.lang.Number containing the integer resource id of the icon
+	 *
+	 * @return List of entries for SimpleAdapter
+	 */
+	private List<Map<String, Object>> getRules() {
+		String [] rules = {
+			"No crying allowed",
+			"When live gives you lemons, make lemonade!",
+			"Sell lemonade for a higher price than it cost you to make it",
+			"Try to earn $100 to win!",
+		};
+		ArrayList list = new ArrayList<>();
+		for(int i = 0 ; i < rules.length; i++) {
+			String rule = rules[i];
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("rule", rule);
+			map.put("index", String.format("%d", i+1));
+			map.put("icon", new Integer(R.drawable.blinky));
+			list.add(map);
+		}
+		return list;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +52,11 @@ public class RulesActivity extends AppCompatActivity {
 		setSupportActionBar(toolbar);
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+		// We need to wire up the R.id.rulesList and bind
+		// the R.id.rulesListIcon and R.id.rulesListRuleText views to
+		// the rules proided by getRules()
+
 	}
 
 }
