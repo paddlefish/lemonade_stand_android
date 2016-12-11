684bea272c82700e471c12c22b6b5f29032b6ed0
Andrew Rahn <andy@iconfactory.com>
2016-12-10 20:36:30 -0600

    Cleanup main activity

--- app/src/main/java/net/paddlefish/lemonadestand/MainActivity.java
+++ app/src/main/java/net/paddlefish/lemonadestand/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import net.paddlefish.lemonadestand.utils.Cancellable;
 import net.paddlefish.lemonadestand.utils.CancellationPool;
@@ -30,8 +32,7 @@ public class MainActivity extends AppCompatActivity implements
 		OutOfMoneyFragment.OnFragmentInteractionListener,
 		DoneSellingFragment.OnFragmentInteractionListener {
 
-	private GameState mGameState;
-	private GameScreen mCurrentScreen;
+	private boolean isActive;
 	private CancellationPool mCancellationPool = new CancellationPool();
 	private final static String PARAM_SAVED_GAME_SCREEN_CODE = "PARAM_SAVED_GAME_SCREEN_CODE";
 	private final static String PARAM_SAVED_GAME_STATE = "PARAM_SAVED_GAME_STATE";
@@ -47,105 +48,31 @@ public class MainActivity extends AppCompatActivity implements
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Log.e("JG", MainActivity.class.getCanonicalName() + ".onCreate was called");
-		Log.e("JG", "onCreate -- savedInstanceState null? - " + (savedInstanceState == null));
+
 		setContentView(R.layout.activity_main);
 
 		Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar); // Attaching the layout to the toolbar object
 		setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
-
-		if (savedInstanceState == null) {
-			switchToScreen(GameScreen.HELLO, new GameModel(null));
-		} else {
-			String code = savedInstanceState.getString(PARAM_SAVED_GAME_SCREEN_CODE);
-			if (code != null) {
-				mCurrentScreen = GameScreen.screenForCode(code);
-			}
-			GameState savedState = savedInstanceState.getParcelable(PARAM_SAVED_GAME_STATE);
-			if (savedState != null) {
-				mGameState = savedState;
-			}
-		}
-	}
-
-	/*
-		The very last callback is onDestroy(). The system calls this method on your activity
-		as the final signal that your activity instance is being completely removed from
-		the system memory.
-
-		Most apps don't need to implement this method -- clean up should be handled in
-		onPause or onStop
-
-		The system calls this method -- you should never call it EXCEPT you must call super's as shown here.
-	 */
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		Log.e("JG", MainActivity.class.getCanonicalName() + ".onDestroy was called");
-	}
-
-	@Override
-	protected void onRestoreInstanceState(Bundle savedInstanceState) {
-		// I put this here in case you want to add an informative Log statement
-		super.onRestoreInstanceState(savedInstanceState);
-		Log.e("JG", MainActivity.class.getCanonicalName() + ".onRestoreInstanceState was called");
-		Log.e("JG", "onRestoreInstanceState -- savedInstanceState null? - " + (savedInstanceState == null));
-	}
-
-	/*
-			The system calls this method when the user is leaving your activity and passes it the
-			Bundle object that will be saved in the event that your activity is destroyed. If the
-			system must recreate the activity instance later, it passes the same Bundle object to
-			both the onRestoreInstanceState() and onCreate() methods
-		*/
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		outState.putParcelable(PARAM_SAVED_GAME_STATE, mGameState);
-		outState.putString(PARAM_SAVED_GAME_SCREEN_CODE, mCurrentScreen.screenCode);
-
-		// Always call the superclass so it can save the view hierarchy state
-		super.onSaveInstanceState(outState);
-		Log.e("JG", MainActivity.class.getCanonicalName() + ".onSaveInstanceState was called");
-	}
-
-	@Override
-	protected void onStart() {
-
-		super.onStart();
-		Log.e("JG", MainActivity.class.getCanonicalName() + ".onStart was called");
-	}
-
-	@Override
-	protected void onStop() {
-
-		super.onStop();
-		Log.e("JG", MainActivity.class.getCanonicalName() + ".onStop was called");
 	}
 
 	@Override
 	protected void onResume() {
-
 		super.onResume();
-		Log.e("JG", MainActivity.class.getCanonicalName() + ".onResume was called");
+		isActive = true;
+		ViewGroup frame = (ViewGroup) findViewById(R.id.placeholder_view);
+		if (frame.getChildCount() == 0) {
+			switchToHelloScreen();
+		}
 	}
 
 	@Override
 	protected void onPause() {
-
 		super.onPause();
-		Log.e("JG", MainActivity.class.getCanonicalName() + ".onPause was called");
+		isActive = false;
 		mCancellationPool.cancel();
 	}
 
-	@Override
-	protected void onRestart() {
-
-		super.onRestart();
-		Log.e("JG", MainActivity.class.getCanonicalName() + ".onRestart was called");
-	}
-
 	private void switchToScreen(GameScreen screen, GameModel gameModel) {
-		mCurrentScreen = screen;
 		switchToScreen(screen, gameModel, 0, 0);
 	}
 
@@ -173,36 +100,35 @@ public class MainActivity extends AppCompatActivity implements
 		return super.onOptionsItemSelected(item);
 	}
 
-	private void switchToScreen(GameScreen screen, GameModel gameModel, int moneyEarned, int glassesWasted) {
+	private boolean switchToScreen(GameScreen screen, GameModel gameModel, int moneyEarned, int glassesWasted) {
 
-		mGameState = gameModel.getGameState();
 		switch (screen) {
 			case HELLO:
-				switchToHelloScreen();
-				break;
+				return switchToHelloScreen();
 			case MONEY:
-				switchToMoneyScreen(gameModel);
-				break;
+				return switchToMoneyScreen(gameModel);
 			case SHOPPING:
-				switchToShoppingScreen(gameModel);
-				break;
+				return switchToShoppingScreen(gameModel);
 			case MAKING:
-				switchToMakeLemonadeScreen(gameModel);
-				break;
+				return switchToMakeLemonadeScreen(gameModel);
 			case DONE_SELLING:
-				switchToDoneSellingScreen(gameModel, moneyEarned, glassesWasted);
-				break;
+				return switchToDoneSellingScreen(gameModel, moneyEarned, glassesWasted);
 			case OUT_OF_MONEY:
-				switchToOutOfMoneyScreen();
-				break;
+				return switchToOutOfMoneyScreen();
 		}
+		return false;
 	}
 
-	private void switchToFragment(Fragment nextFragment) {
-		switchToFragment(nextFragment, true);
+	private boolean switchToFragment(Fragment nextFragment) {
+		return switchToFragment(nextFragment, true);
 	}
 
-	private void switchToFragment(Fragment nextFragment, boolean addToBackStack) {
+	private boolean switchToFragment(Fragment nextFragment, boolean addToBackStack) {
+		if (!isActive) {
+			// The activity is not active; we cannot switch to a new fragment.
+			return false;
+		}
+
 		FragmentManager manager = getSupportFragmentManager();
 		if (!addToBackStack) {
 			for(int i = 0; i < manager.getBackStackEntryCount(); ++i) {
@@ -218,31 +144,31 @@ public class MainActivity extends AppCompatActivity implements
 
 		}
 		transaction.commit();
+
+		return true;
 	}
 
-	void switchToHelloScreen() {
-		mGameState = null;
-		switchToFragment(HelloFragment.newInstance(10000), false);
+	boolean switchToHelloScreen() {
+		return switchToFragment(HelloFragment.newInstance(10000), false);
 	}
-	void switchToOutOfMoneyScreen() {
-		mGameState = null;
-		switchToFragment(OutOfMoneyFragment.newInstance());
+	boolean switchToOutOfMoneyScreen() {
+		return switchToFragment(OutOfMoneyFragment.newInstance());
 	}
 
-	void switchToMoneyScreen(GameModel gameModel) {
-		switchToFragment(MoneyFragment.newInstance(gameModel.getGameState()));
+	boolean switchToMoneyScreen(GameModel gameModel) {
+		return switchToFragment(MoneyFragment.newInstance(gameModel.getGameState()));
 	}
 
-	void switchToShoppingScreen(GameModel gameModel) {
-		switchToFragment(ShoppingFragment.newInstance(gameModel.getGameState()));
+	boolean switchToShoppingScreen(GameModel gameModel) {
+		return switchToFragment(ShoppingFragment.newInstance(gameModel.getGameState()));
 	}
 
-	void switchToMakeLemonadeScreen(GameModel gameModel) {
-		switchToFragment(MakeLemonadeFragment.newInstance(gameModel.getGameState()));
+	boolean switchToMakeLemonadeScreen(GameModel gameModel) {
+		return switchToFragment(MakeLemonadeFragment.newInstance(gameModel.getGameState()));
 	}
 
-	void switchToDoneSellingScreen(GameModel gameModel, int moneyEarned, int glassesWasted) {
-		switchToFragment(DoneSellingFragment.newInstance(gameModel.getGameState(), moneyEarned, glassesWasted));
+	boolean switchToDoneSellingScreen(GameModel gameModel, int moneyEarned, int glassesWasted) {
+		return switchToFragment(DoneSellingFragment.newInstance(gameModel.getGameState(), moneyEarned, glassesWasted));
 	}
 
 	@Override
@@ -263,11 +189,12 @@ public class MainActivity extends AppCompatActivity implements
 		dialog.setCancelable(false);
 		dialog.setIndeterminate(true);
 		dialog.show();
+		final Cancellable progressCancellable = new ProgressDialogCancellable(dialog);
 
 		gameModel.buySome(qtyOrdered, new PurchasingService.PurchaseCompletion() {
 			@Override
 			public void result(boolean success) {
-				dialog.dismiss();
+				progressCancellable.cancel();
 				if (success) {
 					gameModel.makeLemonade();
 					switchToScreen(GameScreen.MAKING, gameModel);
@@ -277,6 +204,7 @@ public class MainActivity extends AppCompatActivity implements
 				}
 			}
 		});
+		mCancellationPool.addCancellable(progressCancellable);
 	}
 
 	@Override
